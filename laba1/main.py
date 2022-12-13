8dec5a8515e34ff06441ec598ef08f5c0e45a014
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-12-13 22:45:59 +0300

    laba 1

---
+++ laba1/main.py
@@ -0,0 +1,27 @@
+from math import sin, cos, tan, pi
+
+
+print('Первая формула')
+for i in range(5):
+    try:
+        a = float(input('Enter a: '))
+        try:
+            z = (sin(a) ** 2 - tan(a) ** 2) / (cos(a) ** 2 - (1 / tan(a)) ** 2)
+            print('Ответ: ', z)
+        except Exception as e:
+            print('ОДЗ')
+    except Exception as e:
+        exit('Введен неверное значение а')
+
+
+print('Вторая формула')
+for i in range(5):
+    try:
+        a = float(input('Enter a: '))
+        try:
+            z = tan(a)**6
+            print('Ответ: ', z)
+        except Exception as e:
+            print('ОДЗ')
+    except Exception as e:
+        exit('Введен неверное значение а')
